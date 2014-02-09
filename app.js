df08dca46eccaeb6edddabad11b461dbc2dcc773
maclandrol <emmanuel.noutahi@hotmail.ca>
2014-02-09 04:54:03 -0500

    Add new element to style and change html code

--- app.js
+++ app.js
@@ -7,14 +7,15 @@ var express = require('express');
 var path = require('path');
 var app =module.exports=express();
 var exec = require('child_process').exec;
-//execute fonction
+
+//execute command with callback as argument
 function execute(command, callback){
     exec(command, function(error, stdout, stderr){ 
     	console.log(stdout);
     	if(error!==null){
     		callback('exec error: '+error);
     	}
-    	else{
+    	else{//Format data into html tag
     		var result= stdout.trim().split('\n').join("</li><li>");
     		if(result){
     			callback("<ol><li>".concat(result, '</li><ol>'));
@@ -29,6 +30,7 @@ function execute(command, callback){
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
+//Configure static path and app specification
 app.configure(function () {
     app.use("/",express.static(path.join(__dirname, 'public')));
   	app.use(express.methodOverride());
@@ -42,37 +44,39 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 }
 
-
+//Listen to data posted on localhost:port/execute
 app.post('/execute', function(req, res){
 	console.log('params: ' + JSON.stringify(req.params));
 	//Get information from data
 	message= req.body.message;
 	myFunc=req.body.func;
 	myFile=req.body.file;
-
+	//Print information ...
 	console.log('body: ' + JSON.stringify(req.body));
 	console.log('query: ' + JSON.stringify(req.query));
 	res.writeHead(200, { 'Content-Type': 'text/plain'}); 
+	//GetCommand output after execution
 	var command=function getCommand(func, file){
-			var command="pwd";
-			if(func==='find'){
-				command="sudo find ~/ -name '*"+file+"*' -print";
+			var task="pwd"; //default task = pwd
+			if(func==='find'){ //case find
+				task="sudo find ~/ -iname '*"+file+"*'";
 			}
-	 			execute(command, function(stdout){
+	 			execute(task, function(stdout){
 	   				res.end(stdout);
 	 			});
 	}
-	if(message==="execute"){
+	if(message==="execute"){//we are execution state
 		console.log('Will execute your message');
 		
 		command(myFunc,myFile);
 	}
-	else {
+	else { //no need to execute
 		console.log('Will not execute your message');
  		res.end('Not executable'+'\n');
   } 
 });
 
+//listen to server on selected port
 app.listen(app.get('port'), function(){
 	console.log("Express server created and listening on port %d in %s mode", app.get('port'), app.settings.env);
 });
